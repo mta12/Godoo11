4a74014b809ba86b55ae9c88c80b2efc62e6b0ac
mta12 <tobiemanga@gmail.com>
2019-05-06 16:54:02 +0100

    ajout du dossier environnement+le dossier initial

---
+++ chap1mta/chap1_installing.py
@@ -0,0 +1,7 @@
+#-*-coding:Utf-8 -*-
+#Démarrons l'installation de l'environnement Odoo dev
+#Il est recommandé d'utiliser une distribution Linux car la majorité des développeurs Odoo
+    #Utilisent Linux.
+#Ubuntu 18.04.1 LTS
+#Gnome 3.28.2
+#Proc 64 bits
